027cca290f7c2948a9b72030584b1bb766c81185
Stefan Stoichev <stefan.stoichev@gmail.com>
2024-11-26 04:58:18 +0000

    s3 dependency update

--- plugins/s3/CHANGELOG.md
+++ plugins/s3/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.1.1] - 2024-11-26
+
+- dependency update
+
 ## [0.1.0] - 2024-11-26
 
 Initial release
--- plugins/s3/src/index.ts
+++ plugins/s3/src/index.ts
@@ -4,7 +4,7 @@ import { NotificationData, S3Callback } from "../../../interfaces";
 
 export const meta = {
   author: "Informatiqal",
-  version: "0.1.0",
+  version: "0.1.1",
   name: "s3",
 };
 
